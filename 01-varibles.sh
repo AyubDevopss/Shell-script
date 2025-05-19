3656c0e2fd6978179a387df6458f246bc6ba1d53
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-05-19 20:36:38 +0530

    hona nybro

---
+++ 01-varibles.sh
@@ -0,0 +1,25 @@
+f[wf-,wf3f'
+flsflfmsf, sf
+s;f'skfplskf
+
+
+ljsfslfm
+s
+fkjnsf
+sfklsnfs
+
+
+sfkoshfioshfksf
+sfiohfko
+fjoisfhsfj
+sfjsopfjspofj
+sfsiofjsfpoj09u3;lf\]pr;lfsfoinsfnwpf
+
+sfpojsfojsfls
+f'lpmwf f\
+;
+s;f mw]f;
+sf;sfnkwkofhiw98hs
+f;s'
+flsnfsiofs''
+f;scS'fs90fuw7f8w
